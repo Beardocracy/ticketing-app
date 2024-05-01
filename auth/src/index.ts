71a859420dbfc09274ab2702840fcb04bf64b72d
“Beardocracy” <travisjbearden@gmail.com>
2024-05-01 15:15:03 -0500

    Created not-found-error for incorrect routes and request types

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,14 +6,19 @@ import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
 app.use(signinRouter);
-app.use(signupRouter);
 app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', () => {
+    throw new NotFoundError();
+})
 
 app.use(errorHandler);
 
