a54f33ace392df29e74f8ba777731dcc64703dc1
“Beardocracy” <travisjbearden@gmail.com>
2024-05-01 13:17:39 -0500

    Added ingress nginx configuration

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+
 const app = express();
 app.use(json());
 
+app.use(currentUserRouter);
+
 app.listen(3000, () => {
-    console.log("Auth server is running on port 3000");
+    console.log("Auth server is running on port 3000!");
 });
