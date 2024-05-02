a0c77fda484fba973e2f4a91ff843f12524ffee3
“Beardocracy” <travisjbearden@gmail.com>
2024-05-02 11:36:19 -0500

    Scaffolding mongo, adding mongoose, modeling user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
-import { json } from "body-parser";
+import {json} from "body-parser";
 import "express-async-errors";
+import mongoose from "mongoose";
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signupRouter } from "./routes/signup";
-import { signoutRouter } from "./routes/signout";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import {currentUserRouter} from "./routes/current-user";
+import {signinRouter} from "./routes/signin";
+import {signupRouter} from "./routes/signup";
+import {signoutRouter} from "./routes/signout";
+import {errorHandler} from "./middlewares/error-handler";
+import {NotFoundError} from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -23,6 +24,18 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("Auth server is running on port 3000!");
-});
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to database');
+    } catch (error) {
+        console.log(error);
+    }
+
+    app.listen(3000, () => {
+        console.log("Auth server is running on port 3000");
+    });
+};
+
+start();
+
