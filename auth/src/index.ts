fbfee35deee03b9f97ec1693c81ee058e1d3df4e
“Beardocracy” <travisjbearden@gmail.com>
2024-05-07 12:47:48 -0500

    Refactored index to have config logic in app.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,52 +1,23 @@
-import express from "express";
-import {json} from "body-parser";
-import "express-async-errors";
-import mongoose from "mongoose";
-import cookieSession from "cookie-session";
+import mongoose from 'mongoose';
 
-import {currentUserRouter} from "./routes/current-user";
-import {signinRouter} from "./routes/signin";
-import {signupRouter} from "./routes/signup";
-import {signoutRouter} from "./routes/signout";
-import {errorHandler} from "./middlewares/error-handler";
-import {NotFoundError} from "./errors/not-found-error";
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true
-    })
-)
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-    throw new NotFoundError();
-})
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
+
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-        console.log('Connected to database');
-    } catch (error) {
-        console.log(error);
+
+        console.log('Connected to MongoDb');
+    } catch (err) {
+        console.error(err);
     }
 
     app.listen(3000, () => {
-        console.log("Auth server is running on port 3000");
+        console.log('Listening on port 3000!!!!!!!!');
     });
 };
 
 start();
-
