762bd38e4a02fed74aee1e65b0b6f5214ca09354
“Beardocracy” <travisjbearden@gmail.com>
2024-05-09 15:20:49 -0500

    Added header, signout

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,23 +1,15 @@
-import axios from "axios"
-import buildClient from "../api/build-client";
+import buildClient from '../api/build-client';
 
-const LandingPage = ({currentUser}) => {
-    console.log('I am in the component', currentUser);
-    return (
-        <div>
-            {currentUser && <p>You are signed in.</p>}
-            <h1>Landing Page</h1>
-        </div>
+const LandingPage = ({ currentUser }) => {
+    return currentUser ? (
+        <h1>You are signed in</h1>
+    ) : (
+        <h1>You are NOT signed in</h1>
     );
 };
 
-/**
- * getInitialProps runs on the server, except for when a user is arriving at the page from an internal link.
- *
- * Since we are making a request, and that request requires a different domain depending on if we're on the server
- * or browser, I'm checking to see where we are.
- */
-LandingPage.getInitialProps = async (context) => {
+LandingPage.getInitialProps = async context => {
+    console.log('LANDING PAGE!');
     const client = buildClient(context);
     const { data } = await client.get('/api/users/currentuser');
 
