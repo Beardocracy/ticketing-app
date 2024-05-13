3588f24d2ca8e0e06b2c3f92537ec2d961437663
“Beardocracy” <travisjbearden@gmail.com>
2024-05-13 12:33:07 -0500

    Added global signin for testing

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,15 +1,12 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 import request from 'supertest';
 import { app } from '../app';
 
-// declare global {
-//     namespace NodeJS {
-//         export interface Global {
-//             signin(): Promise<string[]>;
-//         }
-//     }
-// }
+declare global {
+    var signin: () => string[];
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -36,20 +33,26 @@ afterAll(async () => {
     }
     await mongoose.connection.close();
 });
-//
-// global.signin = async () => {
-//     const email = 'test@test.com';
-//     const password = 'password';
-//
-//     const response = await request(app)
-//         .post('/api/users/signup')
-//         .send({
-//             email,
-//             password,
-//         })
-//         .expect(201);
-//
-// //     const cookie = response.get('Set-Cookie');
-//
-//     return cookie;
-// };
+
+global.signin = () => {
+    // Build a JWT payload.  { id, email }
+    const payload = {
+        id: '1lk24j124l',
+        email: 'test@test.com'
+    };
+
+    // Create the JWT!
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+    // Build session Object. { jwt: MY_JWT }
+    const session = { jwt: token };
+
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session);
+
+    // Take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64');
+
+    // return a string thats the cookie with the encoded data
+    return [`session=${base64}`];
+};
